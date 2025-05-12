4a7a6f642ca633e8affec8f2781e5817691c7773
bridgetorr17 <bridgetorr1902@gmail.com>
2025-05-12 19:53:05 -0400

    basic server routes working

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+// Login/Landing page
+// GET /
+router.get('/', (req, res) => {
+    res.send('Login');
+})
+
+// Dashboard
+// GET /dashboard
+router.get('/dashboard', (req, res) => {
+    res.send('Dashboard');
+})
+
+module.exports = router;
