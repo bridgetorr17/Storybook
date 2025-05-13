38c29fd442bf05ec0b6e298bbe190cac3dce3a87
bridgetorr17 <bridgetorr1902@gmail.com>
2025-05-13 19:45:59 -0400

    google auth and login working

---
+++ routes/auth.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+// Auth with google 
+// GET /auth/google
+router.get('/google', passport.authenticate('google', { scope: ['profile']}))
+
+// Google auth callback
+// GET /auth/google/callback
+router.get('/google/callback', passport.authenticate('google', {failureRedirect: '/'
+}), (req, res) => {
+    res.redirect('/dashboard');
+})
+
+module.exports = router;
