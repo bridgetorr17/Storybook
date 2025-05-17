c2ce83731526b84d2213b23df4ba1f3f26c6c12f
bridgetorr17 <bridgetorr1902@gmail.com>
2025-05-17 18:02:09 -0400

    editing and deleting stories, handlebar helpers

--- routes/stories.js
+++ routes/stories.js
@@ -43,5 +43,126 @@ router.get('/', ensureAuth, async (req, res) => {
     }
 });
 
+//Show single story
+// GET /stories/:id
+router.get('/:id', ensureAuth, async (req, res) => {
+    try{
+        let story = await Story.findById(req.params.id)
+            .populate('user')
+            .lean();
+
+        if(!story){
+            return res.render('error/404')
+        }
+
+        res.render('stories/show', {
+            story
+        })
+    }
+    catch(err){
+        console.log(err);
+        res.render('error/404')
+    }
+});
+
+// Show edit page 
+// GET /stories/edit/:id
+router.get('/edit/:id', ensureAuth, async (req, res) => {
+    try{
+        const story = await Story.findOne({
+            _id: req.params.id
+        }).lean();
+    
+        if(!story){
+            return res.render('error/404');
+        }
+    
+        if(story.user != req.user.id){
+            res.redirect('/stories')
+        }
+        else{
+            res.render('stories/edit', {
+                story,
+            })
+        }
+    }
+    catch(err){
+        console.error(err);
+        return res.render('error/500');
+    }
+});
+
+//Update story
+// PUT /stories/:id
+router.put('/:id', ensureAuth, async (req, res) => {
+    try{
+        let story = await Story.findById(req.params.id).lean();
+
+        if(!story){
+            return res.render('error/404');
+        }
+        
+        if(story.user != req.user.id){
+            res.redirect('/stories')
+        }
+        else{
+            story = await Story.findOneAndUpdate({ _id: req.params.id}, req.body, {
+                new: true,
+                runValidators: true,
+            });
+    
+            res.redirect('/dashboard');
+        }
+    }
+    catch(err){
+        console.error(err);
+        return res.render('error/500');
+    }
+});
+
+//Delete story
+// DELETE /stories/:id
+router.delete('/:id', ensureAuth, async (req, res) => {
+    try{
+        let story = await Story.findById(req.params.id).lean();
+
+        if(!story){
+            return res.render('error/404');
+        }
+
+        if(story.user != req.user.id){
+            res.redirect('/stories')
+        }
+        else{
+            await Story.deleteOne({_id: req.params.id});
+            res.redirect('/dashboard');
+        }
+    }
+    catch(err){
+        console.error(err);
+        return res.render('error/500');
+    }
+});
+
+// User stories
+// GET /stories/user/:userId
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+    try{
+        const stories = await Story.find({
+            user: req.params.userId,
+            status: 'public'
+        })
+            .populate('user')
+            .lean();
+
+        res.render('stories/index', {
+            stories
+        })
+    }
+    catch(err){
+        console.error(err);
+        res.render('error/500')
+    }
+});
 
 module.exports = router;
